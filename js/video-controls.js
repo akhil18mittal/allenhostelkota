bbf20bb72021d8d3bf2f9b592e1854df6d5d6d0d
arwa-5253 <arwaazad2010@gmail.com>
2026-04-16 18:13:49 +0530

    this commit will fix the start/pause btn

--- js/video-controls.js
+++ js/video-controls.js
@@ -3,28 +3,29 @@
  * Adds Play/Pause functionality with button UI updates
  */
 
-(function($) {
+(function ($) {
     "use strict";
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         var video = document.getElementById('hero-video');
         var playBtn = document.getElementById('play-pause-btn');
-        var btnIcon = $('#play-pause-btn i');
 
         if (video && playBtn) {
             var $container = $('.video-hero-container');
+            var $btnIcon = $(playBtn).find('i');
 
             // Initialize state
             if (video.paused) {
                 $container.addClass('is-paused');
-                btnIcon.removeClass('fa-pause').addClass('fa-play');
+                $btnIcon.removeClass('fa-pause').addClass('fa-play');
             } else {
                 $container.removeClass('is-paused');
-                btnIcon.removeClass('fa-play').addClass('fa-pause');
+                $btnIcon.removeClass('fa-play').addClass('fa-pause');
             }
-            
+
             // Toggle play/pause on button click
-            playBtn.addEventListener('click', function() {
+            playBtn.addEventListener('click', function (e) {
+                e.preventDefault();
                 if (video.paused) {
                     video.play();
                 } else {
@@ -32,14 +33,19 @@
                 }
             });
 
-            // Handle video tag native events (in case user uses right click or browser controls)
-            video.addEventListener('play', function() {
-                btnIcon.removeClass('fa-play').addClass('fa-pause');
+            // Handle video tag native events
+            video.addEventListener('play', function () {
+                $btnIcon.removeClass('fa-play').addClass('fa-pause');
                 $container.removeClass('is-paused');
             });
 
-            video.addEventListener('pause', function() {
-                btnIcon.removeClass('fa-pause').addClass('fa-play');
+            video.addEventListener('pause', function () {
+                $btnIcon.removeClass('fa-pause').addClass('fa-play');
+                $container.addClass('is-paused');
+            });
+
+            video.addEventListener('ended', function () {
+                $btnIcon.removeClass('fa-pause').addClass('fa-play');
                 $container.addClass('is-paused');
             });
         }

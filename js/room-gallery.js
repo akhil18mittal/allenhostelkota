b849ede06747a60e6674e2517153db91b9c6e10c
arwa-5253 <arwaazad2010@gmail.com>
2026-04-16 15:16:04 +0530

    this commit will change rooms pics and add start/pause button in intro video

--- js/room-gallery.js
+++ js/room-gallery.js
@@ -1,61 +1,70 @@
-/**
- * Room Gallery Image Zoom
- * Target: Images inside #rooms-section cards
- * Library: Magnific Popup
- */
-
 (function($) {
     "use strict";
 
     $(document).ready(function() {
-        // Only proceed if Magnific Popup is loaded
         if ($.fn.magnificPopup) {
             
-            // Selector for the room images
-            var $roomImages = $('.blog-wrp-layout1 .image-box img');
+            // Define Image Sets for each Room Category
+            var roomGalleries = {
+                0: [ // Deluxe Single Room with Window
+                    { src: 'img/gallery/Deluxe Single Room.jpg', title: 'Deluxe Single Room' },
+                    { src: 'img/gallery/deluxe single room 2.jpg', title: 'Room View' },
+                    { src: 'img/gallery/deluxe single room 3.jpg', title: 'Room Interior' },
+                    { src: 'img/gallery/deluxe_single_room.jpg', title: 'Comfortable Stay' },
+                    { src: 'img/gallery/deulxe_single_room2.jpg', title: 'Window View' },
+                    { src: 'img/gallery/delux washroom.jpg', title: 'Attached Washroom' }
+                ],
+                1: [ // Standard Single Room
+                    { src: 'img/gallery/deluxe single room 1.jpg', title: 'Standard Single Room' },
+                    { src: 'img/gallery/standard_single_room_1.jpg', title: 'Single Room Interior' },
+                    { src: 'img/gallery/standard_single_room_2.jpg', title: 'Room Setup' },
+                    { src: 'img/gallery/deluxe bathroom.jpg', title: 'Bathroom' }
+                ],
+                2: [ // Deluxe Double Room with Window
+                    { src: 'img/gallery/deluxe double room.jpg', title: 'Deluxe Double Room' },
+                    { src: 'img/gallery/delux double room with window 2.jpg', title: 'Double Room Interior' },
+                    { src: 'img/gallery/washroom pic 2.jpg', title: 'Washroom Interior' }
+                ],
+                3: [ // Standard Double Room
+                    { src: 'img/gallery/double room 1.jpg', title: 'Standard Double Room' },
+                    { src: 'img/gallery/double room 2.jpg', title: 'Room Interior' },
+                    { src: 'img/gallery/doubl room bathroom.jpg', title: 'Attached Washroom' },
+                    { src: 'img/gallery/deluxe double room 2.jpg', title: 'Standard Double Room View' }
+                ]
 
+            };
 
-            $roomImages.on('click', function() {
-                var items = [];
-                var startIdx = 0;
-                var clickedSrc = $(this).attr('src');
+            var $roomCards = $('.blog-wrp-layout1');
+
+            $roomCards.each(function(index) {
+                var $img = $(this).find('.image-box img');
                 
-                // Collect all room images into a gallery
-                $roomImages.each(function(idx) {
-                    var src = $(this).attr('src');
-                    var title = $(this).attr('alt') || 'Room Image';
+                $img.on('click', function() {
+                    var items = roomGalleries[index];
                     
-                    items.push({
-                        src: src,
-                        title: title
-                    });
-
-                    // Set starting index to the clicked image
-                    if (src === clickedSrc) {
-                        startIdx = idx;
+                    if (items && items.length > 0) {
+                        $.magnificPopup.open({
+                            items: items,
+                            type: 'image',
+                            gallery: {
+                                enabled: true,
+                                navigateByImgClick: true,
+                                preload: [0, 1]
+                            },
+                            image: {
+                                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                                titleSrc: function(item) {
+                                    return item.title;
+                                }
+                            },
+                            mainClass: 'mfp-fade mfp-img-mobile',
+                            removalDelay: 300
+                        }, 0); // Always start from the first image in the set
                     }
                 });
-                
-                // Open Magnific Popup
-                $.magnificPopup.open({
-                    items: items,
-                    type: 'image',
-                    gallery: {
-                        enabled: true,
-                        navigateByImgClick: true,
-                        preload: [0, 1] // Preload nearby images
-                    },
-                    image: {
-                        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                        titleSrc: function(item) {
-                            return item.title;
-                        }
-                    },
-                    mainClass: 'mfp-fade mfp-img-mobile',
-                    removalDelay: 300, // Sync with CSS transition
-                }, startIdx);
             });
         }
     });
 
 })(jQuery);
+
